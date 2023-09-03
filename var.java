d71ec70750c01ca8493aa60ca464055c89b7147b
charity <wa.nelimacharity@gmail.com>
2023-09-03 18:51:50 +0300

    operators

--- var.java
+++ var.java
@@ -8,3 +8,15 @@ double price = 19.99;     // Double
 char grade = 'A';         // Character
 boolean isJavaFun = true; // Boolean
 String name = "John";     // String
+
+// operators
+
+int a = 10, b = 5;
+int sum = a + b;     // Addition
+int diff = a - b;    // Subtraction
+int product = a * b; // Multiplication
+int quotient = a / b; // Division
+int remainder = a % b; // Modulus (remainder)
+
+boolean isGreater = a > b; // Comparison (greater than)
+boolean isEqual = a == b;  // Comparison (equal to)
