8d04fda63911118094ef18df8385398610fa33a0
charity <wa.nelimacharity@gmail.com>
2023-09-03 18:55:52 +0300

    functions

---
+++ func.java
@@ -0,0 +1,10 @@
+// functions/methods
+
+public int add(int x, int y) {
+    return x + y;
+}
+
+public static void main(String[] args) {
+    int result = add(3, 4);
+    System.out.println("Result: " + result);
+}
