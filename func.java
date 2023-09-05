7bfca1112e1916e531a5bf27b1f9e89040c39440
charity <wa.nelimacharity@gmail.com>
2023-09-05 20:41:32 +0300

    more functions

--- func.java
+++ func.java
@@ -8,3 +8,22 @@ public static void main(String[] args) {
     int result = add(3, 4);
     System.out.println("Result: " + result);
 }
+
+// more examples of functions in java
+
+public class FunctionExample {
+
+    // Define a function (method) named "add" that takes two parameters
+    public static int add(int a, int b) {
+        return a + b; // This method returns the sum of the two parameters
+    }
+
+    // Main method - the entry point of the program
+    public static void main(String[] args) {
+        // Call the "add" method and store the result in a variable
+        int result = add(5, 3);
+
+        // Display the result to the console
+        System.out.println("The result of adding 5 and 3 is: " + result);
+    }
+}
